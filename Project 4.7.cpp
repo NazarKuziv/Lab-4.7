48b93eda707be11678056c008d8d1a31c726df82
Nazar Kuziv <nazar123kuziv@gmail.com>
2020-11-15 15:59:11 +0200

    first commit

---
+++ Project 4.7.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double xp, xk, x, dx, eps, a , R=0 , S;
+	int n;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << "   |"
+		<< setw(9) << "exp(x)" << "  |"
+		<< setw(7) << "S" << "    |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk && x>=-1 && x<1)
+	{
+		n = 1;
+		a = -1; 
+		S = a;
+		do {
+			n++;
+			R = x*(n - 1.) / n;
+			a *= R;
+			S += a;
+		} while (a >= eps);
+
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(4) << exp(x) << " |"
+			<< setw(10) << setprecision(4) << S << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
